7e6583535b910cc8a6516cc5d537c6743d5ec708
KeiraKistensamy <Keirakist0987@gmail.com>
2024-05-14 14:18:25 +0200

    Question2

---
+++ script/code.js
@@ -0,0 +1,13 @@
+// Question 1
+let firstName = "Keira"
+let lastName = "Kistensamy"
+let addition = firstName + lastName 
+console.log(addition)
+console.log(typeof addition)
+
+// Question 2
+let firstName = "Keira"
+let lastName = "Kistensamy"
+let combine = firstName.concat(" ",lastName) 
+console.log(combine)
+console.log(typeof combine)
